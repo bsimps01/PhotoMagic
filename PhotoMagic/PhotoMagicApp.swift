d5f17da73d04b38bb19b2c3e400fad2b9760171f
bsimps01 <b.albertsimpson@gmail.com>
2024-10-11 23:15:11 -0700

    added

---
+++ PhotoMagic/PhotoMagicApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMagicApp.swift
+//  PhotoMagic
+//
+//  Created by Benjamin Simpson on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoMagicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoMagicTests/PhotoMagicTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMagicTests.swift
+//  PhotoMagicTests
+//
+//  Created by Benjamin Simpson on 10/11/24.
+//
+
+import Testing
+@testable import PhotoMagic
+
+struct PhotoMagicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
